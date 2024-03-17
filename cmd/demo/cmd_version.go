d5b1382d205722c01633682a4cf3a13e651de112
László Bukodi <bukodi@gmail.com>
2024-03-17 13:41:20 +0100

    export artifact

--- cmd/demo/cmd_version.go
+++ cmd/demo/cmd_version.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	demo_app "github.com/bukodi/demo-app"
+	"runtime/debug"
 
 	"github.com/spf13/cobra"
 )
@@ -13,8 +14,16 @@ func init() {
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Print the version number",
+	Short: "Print the version number and build info of demo app",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Printf("%s (%s)\n", demo_app.Version, demo_app.GitCommit)
+
+		fmt.Printf("\n")
+
+		bi, ok := debug.ReadBuildInfo()
+		if !ok {
+			panic("ReadBuildInfo failed")
+		}
+		fmt.Printf("Build info:\n%+v\n", bi)
 	},
 }
