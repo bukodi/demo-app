5f7b2dee1b798259d026faba687c40a74fd7c32a
Bukodi László <laszlo.bukodi@noreg.hu>
2024-03-18 18:56:09 +0100

    CI build of AWS lambda handler

--- cmd/demo-aws-lambda/main.go
+++ cmd/demo-aws-lambda/main.go
@@ -6,15 +6,21 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
 	"github.com/bukodi/demo-app/pkg/server"
+	"log"
 )
 
 var lambdaToHttp *httpadapter.HandlerAdapterV2
 
 func Handler(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
-	return lambdaToHttp.ProxyWithContext(ctx, req)
+	log.Printf("Handler called with: %+v\n", req)
+	resp, err := lambdaToHttp.ProxyWithContext(ctx, req)
+	log.Printf("Handler returned with: %+v, %+v\n", resp, err)
+	return resp, err
 }
 
 func main() {
+	log.Println("Starting Lambda log")
 	lambdaToHttp = httpadapter.NewV2(server.ApiV1Mux)
 	lambda.Start(Handler)
+	log.Println("Lambda finished")
 }
