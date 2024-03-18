10911dd2aacfb9917cb4f97be0838d6dd518183f
László Bukodi <bukodi@gmail.com>
2024-03-19 00:35:21 +0100

    Get version from BuildInfo

--- version.go
+++ version.go
@@ -1,5 +1,18 @@
 package demo_app
 
+import "runtime/debug"
+
+func init() {
+	if bi, ok := debug.ReadBuildInfo(); ok {
+		for _, s := range bi.Settings {
+			if s.Key == "vcs.revision" {
+				GitCommit = s.Value
+				break
+			}
+		}
+	}
+}
+
 var (
 	Version   = "0.0.1"
 	GitCommit = "unknown"
