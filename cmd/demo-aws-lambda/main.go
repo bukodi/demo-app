8039f0f948f7a211a59129ffbf052f492a77a84e
László Bukodi <bukodi@gmail.com>
2024-03-18 23:26:40 +0100

    Testing AWS Lambda

--- cmd/demo-aws-lambda/main.go
+++ cmd/demo-aws-lambda/main.go
@@ -6,21 +6,34 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
 	"github.com/bukodi/demo-app/pkg/server"
+	"github.com/kr/pretty"
 	"log"
+	"log/slog"
+	"os"
 )
 
 var lambdaToHttp *httpadapter.HandlerAdapterV2
 
 func Handler(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
-	log.Printf("Handler called with: %+v\n", req)
+	log.Printf("Handler called with: %s\n", pretty.Sprintf("%+v", req))
 	resp, err := lambdaToHttp.ProxyWithContext(ctx, req)
-	log.Printf("Handler returned with: %+v, %+v\n", resp, err)
+	log.Printf("Handler returned with: %+v, %+v\n", pretty.Sprintf("%+v", resp), err)
 	return resp, err
 }
 
 func main() {
+	slog.Info("Starting Lambda slog")
 	log.Println("Starting Lambda log")
+
+	envtxt, err := os.ReadFile("env.txt")
+	if err != nil {
+		log.Printf("Error reading env.txt: %s", err.Error())
+	} else {
+		log.Printf("env.txt: %s", envtxt)
+	}
+
 	lambdaToHttp = httpadapter.NewV2(server.ApiV1Mux)
 	lambda.Start(Handler)
 	log.Println("Lambda finished")
+	slog.Info("Lambda finished slog")
 }
--- go.mod
+++ go.mod
@@ -5,10 +5,13 @@ go 1.22
 require (
 	github.com/aws/aws-lambda-go v1.46.0
 	github.com/awslabs/aws-lambda-go-api-proxy v0.16.1
+	github.com/kr/pretty v0.3.1
 	github.com/spf13/cobra v1.8.0
 )
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/rogpeppe/go-internal v1.9.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 )
