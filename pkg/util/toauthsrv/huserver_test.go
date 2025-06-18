0dd616807ed87460a9bd3dfd548f824d264bcfd0
bukodi <bukodi@gmail.com>
2025-06-19 01:24:00 +0200

    httptest TLS added

--- pkg/util/toauthsrv/huserver_test.go
+++ pkg/util/toauthsrv/huserver_test.go
@@ -38,3 +38,32 @@ func TestHttpUtil(t *testing.T) {
 	}
 	t.Logf("Response readed")
 }
+
+func TestHttpUtilTLS(t *testing.T) {
+	//t.Skipf("Fix this test to finish")
+	//done := make(chan bool)
+
+	srv := httptest.NewUnstartedServer(http.NewServeMux())
+	srv.Listener, _ = net.Listen("tcp", "localhost:9443")
+	srv.StartTLS()
+	defer srv.Close()
+	t.Logf("Server started on %s", srv.URL)
+
+	srv.Config.Handler.(*http.ServeMux).HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello World"))
+		t.Logf("Response sent")
+	})
+
+	resp, err := srv.Client().Get(util.Must(url.JoinPath(srv.URL, "hello")))
+	if err != nil || resp == nil || resp.StatusCode != http.StatusOK {
+		t.Fatalf("Failed to get: %v", err)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	resp.Body.Close()
+	if string(body) != "Hello World" {
+		t.Fatalf("Expected body Hello World, got %s", string(body))
+	}
+	t.Logf("Response readed")
+}
